b6da27187a1ec344fadc24c944509316ee08c5d0
GUILHERME <guilherme.fernandes.f04@gmail.com>
2022-09-10 19:46:03 -0300

    Aulas

---
+++ treino/lista.dart
@@ -0,0 +1,6 @@
+String nome ='gui';
+List<String> ListaNomes = ['gui', 'guigui'];
+List<dynamic> Pessoa=['nome', 20,'321312', "gui"];
+void main() {
+  print("oioioio ${Pessoa[3]}");
+}
---
+++ treino/variaveis.dart
@@ -0,0 +1,42 @@
+///aula de variaveis
+
+void main() {
+
+  ///var String idade ="21" não funciona quando é var
+  ///da na mesma?
+  int idade = 22;
+
+  var jao = "sadasda";
+  var jao1=213;
+  print(jao);
+  const int year = 21;
+  double celcius = 20.312e2;
+  print(celcius);
+
+  ///como mostrar um int com um string
+  ///print(year+idade);
+
+  bool gui = (idade == celcius);
+
+  ///false
+  bool gui1 = (idade == (year + 1));
+
+  ///true
+  bool gui2 = (idade == year);
+
+  ///
+  print(year + idade);
+  print(gui);
+  print(gui1);
+  print(gui2);
+  String oioi = "oioi";
+  String nome = "GUILHERME ERNANDES ";
+  bool gui3 = ("oioi" == "oioi");
+  print(gui3);
+
+  ///precisa do '$' e das aspas
+  print("my name "
+      "is: $nome");
+
+  ///não tem lorem?
+}
